49335355d02e6bf09eb34c82af943824f1f7bd5d
Nathamayil Natesh <nathamayil@gmail.com>
2024-10-27 18:27:02 +0530

    Revert "Extra changes"
    This reverts commit 07ea77ee099549fa57ebd72858618c997d16136c.

--- public/render-page.js
+++ public/render-page.js
@@ -4043,8 +4043,8 @@ var plugins = [{
   plugin: __webpack_require__(/*! ./node_modules/gatsby-plugin-manifest/gatsby-ssr */ "./node_modules/gatsby-plugin-manifest/gatsby-ssr.js"),
   options: {
     "plugins": [],
-    "name": "Nathamayil Natesh",
-    "short_name": "Nathamayil Natesh",
+    "name": "Brittany Chiang",
+    "short_name": "Brittany Chiang",
     "start_url": "/",
     "background_color": "#020c1b",
     "theme_color": "#0a192f",
