3005367e6565ced9dc1660637af7eb6a54b6e002
Nathamayil <nathamayil@gmail.com>
2025-01-19 21:23:24 +0530

    New changes

--- gatsby-config.js
+++ gatsby-config.js
@@ -5,7 +5,7 @@ module.exports = {
     title: 'Nathamayil Natesh',
     description:
       'Nathamayil is a software engineer who specializes in building (and occasionally designing) exceptional digital experiences.',
-    siteUrl: 'https://brittanychiang.com', // No trailing slash allowed!
+    siteUrl: 'https://nathamayilnatesh.netlify.app', // No trailing slash allowed!
     image: '/og.png', // Path to your image you placed in the 'static' folder
     twitterUsername: '@nathamayil07',
   },
@@ -20,8 +20,8 @@ module.exports = {
     {
       resolve: `gatsby-plugin-manifest`,
       options: {
-        name: 'Brittany Chiang',
-        short_name: 'Brittany Chiang',
+        name: 'Nathamayil Natesh',
+        short_name: 'Nathamayil Natesh',
         start_url: '/',
         background_color: config.colors.darkNavy,
         theme_color: config.colors.navy,
